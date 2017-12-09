96d54bf9257a58b189ebd63dbecb258eaf50ea18
JoJo Zheng^C <jojozheng1216@hotmail.com>
2017-12-09 14:20:19 -0700

    added new details on js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,88 @@
 
 // After array has been established (mine is already named wordBank)
 
-var available letters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var availableLetters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wordBank =["bulbasaur", "eevee", "jolteon", "lapras", "pikachu", "squirtle"];
-var chosenWord= "";
-var lettersInChosenWord= [];
-var blankSpaces= 0;
-var blankAndFilled= [];
-var wrongGuesses =[];
 
-var wins= 0;
-var numOfGuessesRemain= 10;
-var guesses =[];
-
-var numOfGuessesRemain= document.getElementById("guessesRemaining");
-
-
-
-// New variable:
-let (var?) randNum = Math.floor(Math.random() * PokemonNames.length);
-
-another new variable:
-let (var?) choosenWord = word[randNum];
-console.log (chosenWord);
-
-and another:
-let (var?) underScore = [];
-console.log (chosenWord);
-
-another two...gets added after "if user's guess is right"
-var guessRight = [];
-var guessWrong = [];
-
-generate underscores based on the legnth of the array (each word):
-var generateUnderscore = () => {
-for (var i = 0; i < chosenWord.length; i++) {
-  console.log(wordBank[i]);
-underScore.push ('_');    ....the "_" mean we will be pushing underScore itself
-
-		}
-	return underScore;
+var pokemonInfo = [
+	{
+		name: "bulbasaur",
+		shadow: "./assets/images/bulbasaur_shadow.png",
+		solved: "./assets/images/bulbasaur.png"
+	},
+	{
+		name: "eevee",
+		shadow: "link",
+		solved: "link",
+	},
+	{
+		name: "jolteon",
+		shadow: "link",
+		solved: "link",
 	}
-	console.log(generateUnderscore());
+]
 
+var currentWord;
+var numberOfWins = 0;
+var guessesRemaining = 5;
+var lettersGuessed = [];
 
-	// Now we have to capture the user's guess:
-document.addEventListener ('keypress', (event) => {
-console.log(event);   
-
-
-// Now we convert the key code into a letter
-var keyCode = event.keycode <----but no..wait, were going to convert that into a string. See
-var KeyWord = String.fromCharCode(keyCode);
-
-// If user's guess is right:
-if (chosenWord.Indexof(Keyword > -1) {
-	rightWord.push(keyWord)
-	wrongWord.push(keyWord)
+function currentWord(){
+	var random = Math.floor(Math.random() * wordBank.length);
+	var current = pokemonInfo[random].name;
+	currentWord = current
+	displayValue("randomWord", makeUnderscore(current));
 }
-});
+
+function displayValue(id, value){
+	document.getElementById(id).innerText = value;
+}
+
+function displayPokemon(pokemonLink) {
+	document.getElementById("guess-that-pokemon").src = pokemonLink;
+}
+
+function makeUnderscore(word){
+	var underscores = [];
+
+	for (var i = 0; i < word.length; i++) {
+		underscores.push("_");
+	}
+
+	return underscores.join(" ");
+}
+
+
+
+//look up .indexof js//
+function checkUserGuess(letter){
+	//check if guess is in current word//
+
+	//if guess is in current word -- correct
+	//display guess 
+	//change letters that have been guessed
+
+	//then 
+	//if guess is not in current word-- incorrect 
+	// display letterguessed
+	// (-) guessed remaining
+	// display one less pokeball
+	// can only loose life once per guessed letter
+	};
+
+
+//On Load Call these
+currentWord();
+displayValue("wins", numberOfWins);
+displayValue("guessesRemaining", guessesRemaining);
+displayValue("lettersGuessed", lettersGuessed);
+
+
+
+// Run game functions on event
+document.onkeyup= function (event){
+	checkUserGuess(event.key);
+}
+
+
+
